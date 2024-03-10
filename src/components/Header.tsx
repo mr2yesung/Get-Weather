a2a17c5fcfc02109aa0ad1a51e22fbf8aabc5513
mr2yesung <mryesung@naver.com>
2024-03-10 20:55:33 +0900

    adjust title get weather to link to relative origin path when clicked

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header({ children }: HeaderProps): JSX.Element {
   return (
     <div className="flex flex-row items-center justify-between gap-x-3 px-4 py-6">
       <h1 className="whitespace-nowrap text-lg font-semibold leading-none tracking-tight md:text-2xl lg:text-3xl xl:text-xl">
-        Get Weather
+        <a href="/">Get Weather</a>
       </h1>
       {children}
     </div>
